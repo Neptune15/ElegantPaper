b8c6b6d9a62096bd1d5214b7d9d4375dd419e9e5
Neptune15 <yueshangzuo@outlook.com>
2019-06-13 18:12:48 +0800

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex\(busy\)
+
+
+rm *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
+
